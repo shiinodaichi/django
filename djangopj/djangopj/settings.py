d61be516de61fef063db4530725259a994c4648d
daichi <shiinodaichi@gmail.com>
2019-10-22 19:19:06 +0900

    hello world

--- djangopj/djangopj/settings.py
+++ djangopj/djangopj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'myapp.apps.MyappConfig', #myapp アプリケーションのMyappConfigの指定
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
